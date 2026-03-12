61dd1058564c201f93970cdea5b30b0b9db78dd3
Daniel Rodriguez <daniel.rodriguez@udr.com>
2026-03-12 13:52:24 -0600

    Add optional targetDuration to Script model

--- Teleprompter/Models/Script.swift
+++ Teleprompter/Models/Script.swift
@@ -9,6 +9,7 @@ final class Script {
     var modifiedAt: Date
     var scrollSpeed: Double
     var fontSize: Double
+    var targetDuration: Double?
 
     var sortedSections: [ScriptSection] {
         sections.sorted { $0.order < $1.order }
@@ -18,7 +19,8 @@ final class Script {
         name: String,
         sections: [ScriptSection] = [],
         scrollSpeed: Double = 1.0,
-        fontSize: Double = 16.0
+        fontSize: Double = 16.0,
+        targetDuration: Double? = nil
     ) {
         self.name = name
         self.sections = sections
@@ -26,5 +28,6 @@ final class Script {
         self.modifiedAt = Date.now
         self.scrollSpeed = scrollSpeed
         self.fontSize = fontSize
+        self.targetDuration = targetDuration
     }
 }
